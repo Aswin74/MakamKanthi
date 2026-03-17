d5641170df8a17a397190c78640048e7fddbc7f6
Aswin74 <jayaprakash.jp713@gmail.com>
2026-03-17 08:32:49 +0530

    Navbar add

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const navLinks = [
   { label: "Home", to: "/" },
   { label: "Services", to: "#services" },
   { label: "About", to: "#about" },
-  //   { label: "Contact", to: "#contact" },
+  { label: "Works", to: "#works" },
 ]
 
 const Navbar = () => {
